cd477584bd463b18ab1365eacb1414aea19845be
OzBoz <ozairboss2005@gmail.com>
2025-10-12 13:06:54 -0400

    Moved Big Strings to a seperate file

--- src/AugmentContent.py
+++ src/AugmentContent.py
@@ -3,6 +3,7 @@ from ollama import chat
 from ollama import Client
 from dotenv import load_dotenv
 import os
+import PromptsAndStuff
 from ollama import ChatResponse
 
 def get_extra_content(company_name:str, extra_considerations:str) -> str:
@@ -10,37 +11,7 @@ def get_extra_content(company_name:str, extra_considerations:str) -> str:
     response:ChatResponse = chat(model="gemma3:4b", messages=[
         {
             "role" : "system",
-            "content" : f"""You are the Outreach Director for the Association of Computing Machinery (ACM) student club at
-            the University of Central Florida.
-            
-            Email Content:
-            Good Afternoon, CONTACT
-
-            I hope this email finds you well. My name is DIRECTOR, and I am the Outreach Director for the Association of Computing Machinery (ACM) student chapter at the University.
-
-            ACM is a worldwide organization built on the foundation of computer engineering and computer science, focused on providing resources to advance the field and profession. We seek to replicate that mission here at our ACM student chapter, leaving a long-lasting impact on UCF's research ecosystem and supporting students through their growth of technical, leadership, and soft skills.
-
-            [YOUR PERSONAL TOUCH HERE]
-
-            Thank you for your time, and I look forward to your response.
-            
-            Given the company you are asked about, you must research information about their values and work, and write
-            in a persuasive tone about how their values align with ours and how their field of technology would
-            provide a great opportunity for students, as well as networking opportunities.
-            We are specifically inviting them to speak at one of our General Body Meetings.
-
-            There may also be some extra considerations, such as us having a past history with the company or lab in question.
-            Be sure to bring up this information in your response.
-
-            ACM's Values: At ACM, we value student involvement and choice. We want our students to be involved, and want to
-            foster a culture of knowledge. We host workshops, work on projects, and want to provide good networking opportunities for students.
-
-            Example personal touches:
-            We would love to hear insights into the world of Fintech from top engineers in the industry!
-
-            This would provide a great opportunity for students to learn more about the tech industry and AI, providing a great opportunity for learning and networking.  
-            We would love to have a representative speak at one of our General Body Meetings and give their insights!
-            """
+            "content" : PromptsAndStuff.SYSTEM_PROMPT
         },
         {
             "role" : "user",
@@ -49,13 +20,18 @@ def get_extra_content(company_name:str, extra_considerations:str) -> str:
             Only write the PERSONAL TOUCH part, no subject line, no greeting, that's already written.
             This should be a complete response.
             Keep it short but sweet (no more than 2-3 sentences).
+            Unless you are explicitly told that we had a prior collaboration, assume this is the first time we are communicating.
             """
         }
-    ])
+    ],
+    options = {
+        "temperature" : 0.2
+    }
+    )
     return str(response.message.content)
 
 def main():
-    print(get_extra_content("Google", "We held interview workshops last year and students enjoyed those"))
+    print(get_extra_content("Lockheed Martin", "Many of our students speak highly about Lockheed's CWEP program"))
 
 if __name__ == "__main__":
     main()
--- src/MailFlower.py
+++ src/MailFlower.py
@@ -1,11 +1,11 @@
 import base64
-import src.AugmentContent as AugmentContent
+import AugmentContent
 from email.mime.application import MIMEApplication
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os.path
-
+import PromptsAndStuff
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -15,8 +15,6 @@ from googleapiclient.errors import HttpError
 # If modifying these scopes, delete the file token.json.
 SCOPES = ["https://www.googleapis.com/auth/gmail.compose"]
 
-SENDER_NAME = "Ozair Ahmed"
-SENDER_POSITION = "Outreach Director"
 
 class MessageContent():
     def __init__(self, email:str, firstname:str, lastname:str, title:str, company:str, considerations:str):
@@ -44,7 +42,7 @@ def main():
 
 def create_draft(service, csv_line:str):
     # Message content class
-    content = MessageContent("ozairboss2005@gmail.com", "Ozair", "Ahmed", "Mr.", "OzBoz Industries", "")
+    content = MessageContent("ozairboss2005@gmail.com", "John", "Doe", "Mr.", "Lockheed Martin", "No extra considerations")
 
     # Open ACM Logo file as binary
     with open("logo.png", "rb") as f:
@@ -72,11 +70,11 @@ def create_message(content: MessageContent, logo_data, presentation_data) -> str
     # Multipart allows putting multiple types of data
     message = MIMEMultipart("related")
     message["To"] = f"{content.email}"
-    message["From"] = "ozairboss2005@gmail.com"
+    message["From"] = PromptsAndStuff.EMAIL_ADDRESS
     message["Subject"] = f"{content.company} x ACM@UCF Collaboration"
 
     # HTML Body content
-    message_html = MIMEText(create_body_text(content, AugmentContent.get_extra_content(content.company, content.considerations)), "html")
+    message_html = MIMEText(PromptsAndStuff.create_body_text(content, AugmentContent.get_extra_content(content.company, content.considerations)), "html")
     message.attach(message_html)
 
     # Inlined logo image
@@ -95,35 +93,7 @@ def create_message(content: MessageContent, logo_data, presentation_data) -> str
     encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
     return encoded_message
 
-# Wow I love HTML
-def create_body_text(c: MessageContent, extra_content:str) -> str:
-    return  f"""
-    <html>
-    <body>
-    <p><span style="font-family: Georgia, serif; font-size: 11pt;">Good Morning, {c.title} {c.lastname},</p>
-    <p><span style="font-family: Georgia, serif; font-size: 11pt;">
-    I hope this message finds you well. My name is {SENDER_NAME}, and I am the {SENDER_POSITION} 
-    for the Association of Computing Machinery (ACM) student chapter at the University of Central Florida.
-    </span></p>
-    <p><span style="font-family: Georgia, serif; font-size: 11pt;">
-    ACM is a worldwide organization built on the foundation of computer engineering and computer science, focused on providing resources to advance the field and profession. 
-    We seek to replicate that mission here at our ACM student chapter, leaving a long-lasting impact on UCF's research ecosystem and 
-    supporting students through their growth of technical, leadership, and soft skills.
-    </span></p>
-    <p><span style="font-family: Georgia, serif; font-size: 11pt;">
-    {extra_content}
-    </span></p>
-    <br>
-    <p><span style="font-family: Georgia, serif; font-size: 11pt;">Thank you for your time, and I look forward to your response.</span></p>
-    <br>
-    <p><span style="font-family: Georgia, serif; font-size: 12pt;">Regards,</span></p>
-    <p><span style="font-family: Georgia, serif; font-size: 16pt;"><b>{SENDER_NAME}</b></p></span>
-    <p><span style="font-family: Georgia, serif; font-size: 14pt;"><b><i>{SENDER_POSITION}</i></b></p>
-    <p><span style="font-family: Georgia, serif; font-size: 12pt;">Association of Computing Machinery, UCF Student Branch</p>
-    <img src="cid:logo" alt="ACM Logo">
-    </body>
-    </html> 
-"""
+
 
 # From the docs
 def validate_creds():
